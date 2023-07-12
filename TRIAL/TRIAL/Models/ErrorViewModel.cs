96149db4ffbb76047844458bc8a75d88358bfdfe
Roel Christopher Dalonos <ghooqmail@gmail.com>
2023-07-12 11:00:44 +0800

    Add project files.

---
+++ TRIAL/TRIAL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TRIAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebApplication7/Models/FileUploadModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace MvcApplication1.Models
+{
+    public class FileUploadModel
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+    }
+}
