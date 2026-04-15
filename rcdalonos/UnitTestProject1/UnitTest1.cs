bc05dd231fe7f3038757fcdd30b2e8637192784f
RC DALONOS <139296232+rcdalonos@users.noreply.github.com>
2026-04-15 17:45:33 +0800

    c

---
+++ rcdalonos/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+
+    class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    [TestClass]
+    public class CalcUnitTest1
+    {
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            Calculator calc = new Calculator();
+            int num1 = 1, num2 = 2;
+            var result = calc.Add(num1, num2);
+            //result = 4;
+            Assert.AreEqual(num1 + num2, result);
+        }
+
+    }
+}
